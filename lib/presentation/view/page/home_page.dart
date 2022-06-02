858a3c469ec4d6569bec3b7368478ce42171aee9
Kodai Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-02 09:07:16 +0900

    Update home_page.dart

--- lib/presentation/view/page/home_page.dart
+++ lib/presentation/view/page/home_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 /// ホーム画面
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentTab = ref.watch(botto);
+    return Scaffold(
+      body: SafeArea(
+        child: IndexedStack(
+          children: [],
+          index: ,
+        ),
+      ),
+    );
   }
 }
