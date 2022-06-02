e60f8422663000b59e474b7a305a0093593c0f5b
Kodai Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-02 10:00:42 +0900

    Update certification_list.dart

--- lib/presentation/view/organism/certification_list.dart
+++ lib/presentation/view/organism/certification_list.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:gcp_certification_exercise/presentation/controller/certification_list_controller.dart';
+import 'package:gcp_certification_exercise/presentation/view/organism/certification.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 /// 資格一覧
-class CertificationList extends StatelessWidget {
+class CertificationList extends ConsumerWidget {
   const CertificationList({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncValue = ref.watch(certificationListController);
+    return asyncValue.when(
+      data: (data) {
+        final certifications = data.certifications;
+        return ListView(
+          children: [
+            Certification(),
+            Certification(),
+            Certification(),
+          ],
+        );
+      },
+      error: (error, _) => Container(),
+      loading: () => Container(),
+    );
   }
 }
