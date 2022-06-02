8b23471b2839d4570a62443a973d7d9b39090943
Kodai Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-02 09:21:47 +0900

    Update home_page.dart

--- lib/presentation/view/page/home_page.dart
+++ lib/presentation/view/page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gcp_certification_exercise/presentation/controller/bottom_navigation_controller.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 /// ホーム画面
@@ -7,13 +8,30 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final currentTab = ref.watch(botto);
+    final controller = ref.read(bottomNavigationController.notifier);
+    final currentTab = ref.watch(bottomNavigationController);
     return Scaffold(
       body: SafeArea(
         child: IndexedStack(
-          children: [],
-          index: ,
+          children: [
+            Container(),
+            Container(),
+          ],
+          index: currentTab.index,
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentTab.index,
+        onTap: (index) =>
+            controller.selectTab(BottomNavigationItem.values[index]),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+          ),
+        ],
       ),
     );
   }
