38aa1f764fffe12f517e72c240fc9d2f6f55a828
Kodai Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-02 17:41:57 +0900

    Update question.dart

--- lib/presentation/view/organism/question.dart
+++ lib/presentation/view/organism/question.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gcp_certification_exercise/presentation/view/molecule/see_answer_button.dart';
 
 /// 問題
 class Question extends StatelessWidget {
@@ -6,6 +7,23 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Flexible(
+          child: SingleChildScrollView(
+            child: Text(
+              '組織がクラウド導入の成熟レベルを「戦略的」から「変革的」に移行したいと考えています。 スケーリングの手法をどのように変更すべきですか。\n'
+              'A. 変更をプログラムでデプロイする。\n'
+              'B. 変更を手作業で確認する。\n'
+              'C. 問題が発生したら変更をデプロイする。\n',
+            ),
+          ),
+        ),
+        SeeAnswerButton(
+          onPressed: () {},
+        ),
+      ],
+    );
   }
 }
