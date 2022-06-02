59b2d79f70c5218c9952b41361f7236eaeee3181
Kodai Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-02 22:35:06 +0900

    Update course_controller.dart

--- lib/presentation/controller/course_controller.dart
+++ lib/presentation/controller/course_controller.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:gcp_certification_exercise/presentation/util/navigator_util.dart';
 import 'package:gcp_certification_exercise/presentation/view/organism/course.dart';
 import 'package:gcp_certification_exercise/presentation/view/page/certification_list_page.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final courseControllers =
-    StateNotifierProvider.family<CourseController, String, AsyncValue<Course>>(
+    StateNotifierProvider.family<CourseController, AsyncValue<Course>, String>(
   (ref, id) => CourseController(),
 );
 
@@ -15,12 +16,9 @@ class CourseController extends StateNotifier<AsyncValue<Course>> {
   Future<void> showCertificationList({
     required BuildContext context,
   }) async {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) {
-          return const CertificationListPage();
-        },
-      ),
+    NavigatorUtil.push(
+      context: context,
+      page: const CertificationListPage(),
     );
   }
 }
