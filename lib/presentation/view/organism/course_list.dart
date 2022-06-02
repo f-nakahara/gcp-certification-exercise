8c8d5e60e70873b449aaf28fd3c3f3e18d3e418d
Kodai Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-02 09:10:34 +0900

    Update course_list.dart

--- lib/presentation/view/organism/course_list.dart
+++ lib/presentation/view/organism/course_list.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:gcp_certification_exercise/presentation/controller/course_list_controller.dart';
+import 'package:gcp_certification_exercise/presentation/view/organism/course.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 /// コース一覧
-class CourseList extends StatelessWidget {
+class CourseList extends ConsumerWidget {
   const CourseList({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncValue = ref.watch(courseListController);
+    return asyncValue.when(
+      data: (data) {
+        final courses = data.courses;
+        return ListView(
+          children: [
+            Course(),
+            Course(),
+            Course(),
+          ],
+        );
+      },
+      error: (error, _) => Container(),
+      loading: () => Container(),
+    );
   }
 }
