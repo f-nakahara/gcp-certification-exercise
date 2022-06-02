2bb0466469fb467b9b3045dfc912f77567579c6b
Kodai Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-02 23:31:22 +0900

    Update certification_controller.dart

--- lib/presentation/controller/certification_controller.dart
+++ lib/presentation/controller/certification_controller.dart
@@ -1,11 +1,31 @@
+import 'package:flutter/material.dart';
 import 'package:gcp_certification_exercise/domain/certification.dart';
+import 'package:gcp_certification_exercise/presentation/util/navigator_util.dart';
+import 'package:gcp_certification_exercise/presentation/view/page/question_page.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'bottom_navigation_controller.dart';
 
 final certificationControllers = StateNotifierProvider.family<
     CertificationController, AsyncValue<Certification>, String>(
-  (ref, id) => CertificationController(),
+  (ref, id) => CertificationController(reader: ref.read),
 );
 
 class CertificationController extends StateNotifier<AsyncValue<Certification>> {
-  CertificationController() : super(const AsyncLoading());
+  CertificationController({
+    required this.reader,
+  }) : super(const AsyncLoading());
+
+  final Reader reader;
+
+  Future showQuestionPage({
+    required BuildContext context,
+  }) async {
+      NavigatorUtil.push(
+        context: reader(bottomNavigationController.notifier)
+            .currentState
+            .context,
+        page: const QuestionPage(),
+      );
+  }
 }
