6dd676ddd5f3065bfddd0ec17e152280d3146417
Kodai Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-02 23:31:20 +0900

    Update bottom_navigation_controller.dart

--- lib/presentation/controller/bottom_navigation_controller.dart
+++ lib/presentation/controller/bottom_navigation_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final bottomNavigationController =
@@ -12,6 +13,15 @@ enum BottomNavigationItem {
 
 class BottomNavigationController extends StateNotifier<BottomNavigationItem> {
   BottomNavigationController() : super(BottomNavigationItem.courseList);
+
+  final rootNavigatorKey = GlobalKey<NavigatorState>();
+
+  final Map<BottomNavigationItem, GlobalKey<NavigatorState>> navigatorKeys = {
+    BottomNavigationItem.courseList: GlobalKey<NavigatorState>(),
+    BottomNavigationItem.wordList: GlobalKey<NavigatorState>(),
+  };
+
+  NavigatorState get currentState => navigatorKeys[state]!.currentState!;
 
   void selectTab(BottomNavigationItem item) {
     state = item;
