95545f3063a14a70bf2728ff88327b167b757ec2
Kodai Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-02 09:10:25 +0900

    Update course.dart

--- lib/presentation/view/organism/course.dart
+++ lib/presentation/view/organism/course.dart
@@ -4,8 +4,17 @@ import 'package:flutter/material.dart';
 class Course extends StatelessWidget {
   const Course({Key? key}) : super(key: key);
 
+  void onTap() {}
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: ListTile(
+        title: Text('Course Name'),
+        leading: Icon(Icons.folder),
+        onTap: () {},
+      ),
+    );
   }
 }
