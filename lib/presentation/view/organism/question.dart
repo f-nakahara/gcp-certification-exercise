2dc4fc274e4eb34b411a2534ab255ba9b8bef676
Kodai Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-02 22:04:08 +0900

    Update question.dart

--- lib/presentation/view/organism/question.dart
+++ lib/presentation/view/organism/question.dart
@@ -12,11 +12,21 @@ class Question extends StatelessWidget {
       children: [
         Flexible(
           child: SingleChildScrollView(
-            child: Text(
-              '組織がクラウド導入の成熟レベルを「戦略的」から「変革的」に移行したいと考えています。 スケーリングの手法をどのように変更すべきですか。\n'
-              'A. 変更をプログラムでデプロイする。\n'
-              'B. 変更を手作業で確認する。\n'
-              'C. 問題が発生したら変更をデプロイする。\n',
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                /// 問題文
+                Text(
+                    '組織がクラウド導入の成熟レベルを「戦略的」から「変革的」に移行したいと考えています。 スケーリングの手法をどのように変更すべきですか。\n'),
+                SizedBox(height: 10),
+
+                /// 選択肢
+                Text(
+                  'A. 変更をプログラムでデプロイする。\n'
+                  'B. 変更を手作業で確認する。\n'
+                  'C. 問題が発生したら変更をデプロイする。\n',
+                ),
+              ],
             ),
           ),
         ),
