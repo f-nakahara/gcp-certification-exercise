e5ef72f5c9390271d9b0839820dd7ce3b87f1274
Kodai Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-02 22:04:06 +0900

    Update question.dart

--- lib/domain/question.dart
+++ lib/domain/question.dart
@@ -4,11 +4,13 @@ import 'package:equatable/equatable.dart';
 class Question extends Equatable {
   final String id;
   final String body;
+  final String choices;
   final String explanation;
 
   const Question({
     required this.id,
     required this.body,
+    required this.choices,
     required this.explanation,
   });
 
