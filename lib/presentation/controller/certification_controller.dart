df9507f1937b58621379dfba9b484c7bd1e78803
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-05 03:01:04 +0900

    Update certification_controller.dart

--- lib/presentation/controller/certification_controller.dart
+++ lib/presentation/controller/certification_controller.dart
@@ -1,31 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:gcp_certification_exercise/domain/certification.dart';
+import 'package:gcp_certification_exercise/presentation/controller/certification_list_controller.dart';
 import 'package:gcp_certification_exercise/presentation/util/navigator_util.dart';
 import 'package:gcp_certification_exercise/presentation/view/page/question_page.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'bottom_navigation_controller.dart';
-
-final certificationControllers = StateNotifierProvider.family<
+final certificationController = StateNotifierProvider.family<
     CertificationController, AsyncValue<Certification>, String>(
-  (ref, id) => CertificationController(reader: ref.read),
+  (ref, id) => CertificationController(
+    reader: ref.read,
+    id: id,
+  ),
 );
 
 class CertificationController extends StateNotifier<AsyncValue<Certification>> {
   CertificationController({
     required this.reader,
-  }) : super(const AsyncLoading());
+    required this.id,
+  }) : super(const AsyncLoading()) {
+    _fetch();
+  }
 
   final Reader reader;
+  final String id;
+
+  Future<void> _fetch() async {
+    final certification = reader(certificationListController)
+        .value!
+        .certifications
+        .singleWhere((element) => element.id == id);
+    state = AsyncData(certification);
+  }
 
   Future showQuestionPage({
     required BuildContext context,
   }) async {
-      NavigatorUtil.push(
-        context: reader(bottomNavigationController.notifier)
-            .currentState
-            .context,
-        page: const QuestionPage(),
-      );
+    NavigatorUtil.push(
+      context: context,
+      page: const QuestionPage(),
+    );
   }
 }
