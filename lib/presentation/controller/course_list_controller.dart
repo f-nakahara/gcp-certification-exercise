eb7415840d2ec8ad6e541c15a1495f67bb2483ba
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-05 02:27:07 +0900

    Update course_list_controller.dart

--- lib/presentation/controller/course_list_controller.dart
+++ lib/presentation/controller/course_list_controller.dart
@@ -1,24 +1,26 @@
 import 'package:gcp_certification_exercise/domain/course_list.dart';
+import 'package:gcp_certification_exercise/infrastructure/gcp_certification_exercise_api/gcp_certification_exercise_api.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final courseListController =
     StateNotifierProvider<CourseListController, AsyncValue<CourseList>>(
-  (ref) => CourseListController(),
+  (ref) => CourseListController(reader: ref.read),
 );
 
 class CourseListController extends StateNotifier<AsyncValue<CourseList>> {
-  CourseListController() : super(const AsyncLoading()) {
+  CourseListController({
+    required this.reader,
+  }) : super(const AsyncLoading()) {
     _fetch();
   }
+
+  final Reader reader;
 
   Future<void> _fetch({
     bool reload = false,
   }) async {
-    state = AsyncData(
-      CourseList(
-        courses: [],
-      ),
-    );
+    final result = await reader(gcpCertificationExerciseAPI).courses();
+    state = AsyncData(result);
   }
 
   Future<void> reload() async {
