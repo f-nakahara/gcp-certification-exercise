540129cde023d1a52264c696aeb818e5e149b883
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-05 03:01:02 +0900

    Update gcp_certification_exercise_api.dart

--- lib/infrastructure/gcp_certification_exercise_api/gcp_certification_exercise_api.dart
+++ lib/infrastructure/gcp_certification_exercise_api/gcp_certification_exercise_api.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:gcp_certification_exercise/domain/certification_list.dart';
 import 'package:gcp_certification_exercise/domain/course_list.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -8,7 +9,7 @@ final gcpCertificationExerciseAPI = Provider<GCPCertificationExerciseAPI>(
 
 class GCPCertificationExerciseAPI {
   static const api =
-      'https://script.google.com/macros/s/AKfycbwRRDcaDNlcgedXXbP625LJBdmsfviWSKIFQME7bnonsZ5C5Ub823IWyKsvNmSdCcOs/exec';
+      'https://script.google.com/macros/s/AKfycbzo7ByCGaHSktjsZ7wwMu8otLGmbN7NNsBA92iXrSo0km79lX9Fp8VxaCBu7Tnd1KX3/exec';
   final dio = Dio(BaseOptions(
     responseType: ResponseType.json,
     baseUrl: api,
@@ -18,6 +19,12 @@ class GCPCertificationExerciseAPI {
   Future<CourseList> courses() async {
     final response = await dio.get('$api?path=courses');
     final data = CourseList.fromMap(response.data);
+    return data;
+  }
+
+  Future<CertificationList> certifications() async {
+    final response = await dio.get('$api?path=certifications');
+    final data = CertificationList.fromMap(response.data);
     return data;
   }
 }
