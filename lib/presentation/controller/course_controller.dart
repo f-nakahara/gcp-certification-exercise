90cc7ec66e6f70a07135518818b235f7a4756113
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-05 02:27:03 +0900

    Update course_controller.dart

--- lib/presentation/controller/course_controller.dart
+++ lib/presentation/controller/course_controller.dart
@@ -1,31 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:gcp_certification_exercise/domain/course.dart';
+import 'package:gcp_certification_exercise/presentation/controller/course_list_controller.dart';
 import 'package:gcp_certification_exercise/presentation/util/navigator_util.dart';
-import 'package:gcp_certification_exercise/presentation/view/organism/course.dart';
 import 'package:gcp_certification_exercise/presentation/view/page/certification_list_page.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'bottom_navigation_controller.dart';
-
 final courseControllers =
     StateNotifierProvider.family<CourseController, AsyncValue<Course>, String>(
-  (ref, id) => CourseController(reader: ref.read),
+  (ref, id) => CourseController(reader: ref.read, id: id),
 );
 
 class CourseController extends StateNotifier<AsyncValue<Course>> {
   CourseController({
     required this.reader,
-  }) : super(const AsyncLoading());
+    required this.id,
+  }) : super(const AsyncLoading()) {
+    _fetch();
+  }
 
   final Reader reader;
+  final String id;
+
+  Future<void> _fetch() async {
+    final course = reader(courseListController)
+        .value!
+        .courses
+        .singleWhere((element) => element.id == id);
+    state = AsyncData(course);
+  }
 
   /// 資格一覧の表示
-  Future<void> showCertificationListPage() async {
+  Future<void> showCertificationListPage({
+    required BuildContext context,
+  }) async {
     NavigatorUtil.push(
-      context: reader(bottomNavigationController.notifier).currentState.context,
+      context: context,
       page: const CertificationListPage(),
     );
-    // NavigatorUtil.push(
-    //   context: context,
-    //   page: const CertificationListPage(),
-    // );
   }
 }
