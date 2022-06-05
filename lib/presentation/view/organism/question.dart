4dfcfdc6d53aad2197d3c3da28efd5ccc2c79c70
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-05 14:39:18 +0900

    Update question.dart

--- lib/presentation/view/organism/question.dart
+++ lib/presentation/view/organism/question.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:gcp_certification_exercise/presentation/controller/question_controller.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 /// 問題
-class Question extends StatelessWidget {
-  const Question({Key? key}) : super(key: key);
+class Question extends ConsumerWidget {
+  const Question({
+    Key? key,
+    required this.id,
+  }) : super(key: key);
+
+  final String id;
 
   @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          /// 問題文
-          Text(
-              '組織がクラウド導入の成熟レベルを「戦略的」から「変革的」に移行したいと考えています。 スケーリングの手法をどのように変更すべきですか。\n'),
-          SizedBox(height: 10),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncValue = ref.watch(questionControllers(id));
+    return asyncValue.when(
+      data: (data) {
+        return SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /// 問題文
+              Text(data.body),
+              const SizedBox(height: 10),
 
-          /// 選択肢
-          Text(
-            'A. 変更をプログラムでデプロイする。\n'
-            'B. 変更を手作業で確認する。\n'
-            'C. 問題が発生したら変更をデプロイする。\n',
+              /// 選択肢
+              Text(data.choices),
+            ],
           ),
-        ],
-      ),
+        );
+      },
+      error: (error, _) => Container(),
+      loading: () => Container(),
     );
   }
 }
