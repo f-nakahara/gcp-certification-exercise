33900b605e31614e50bdb927e2d0e5bd13d2d364
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-06-05 14:39:10 +0900

    Update question_controller.dart

--- lib/presentation/controller/question_controller.dart
+++ lib/presentation/controller/question_controller.dart
@@ -1,29 +1,43 @@
+import 'package:flutter/material.dart';
 import 'package:gcp_certification_exercise/domain/question.dart';
-import 'package:gcp_certification_exercise/presentation/controller/bottom_navigation_controller.dart';
+import 'package:gcp_certification_exercise/presentation/controller/question_list_controller.dart';
 import 'package:gcp_certification_exercise/presentation/util/navigator_util.dart';
 import 'package:gcp_certification_exercise/presentation/view/page/answer_explanation_page.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final questionControllers = StateNotifierProvider.family<QuestionController,
     AsyncValue<Question>, String>(
-  (ref, id) => QuestionController(reader: ref.read),
+  (ref, id) => QuestionController(
+    reader: ref.read,
+    id: id,
+  ),
 );
 
 class QuestionController extends StateNotifier<AsyncValue<Question>> {
   QuestionController({
     required this.reader,
+    required this.id,
   }) : super(const AsyncLoading()) {
     _fetch();
   }
 
   final Reader reader;
+  final String id;
 
-  Future<void> _fetch() async {}
+  Future<void> _fetch() async {
+    final question = reader(questionListController)
+        .value!
+        .questions
+        .singleWhere((element) => element.id == id);
+    state = AsyncData(question);
+  }
 
-  Future showAnswerExplanationPage() async {
+  Future showAnswerExplanationPage({
+    required BuildContext context,
+  }) async {
     NavigatorUtil.push(
-      context: reader(bottomNavigationController.notifier).currentState.context,
-      page: const AnswerExplanationPage(),
+      context: context,
+      page: AnswerExplanationPage(questionId: id),
       fullscreenDialog: true,
       rootNavigator: true,
     );
